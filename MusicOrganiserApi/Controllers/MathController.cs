f76b2c5bec823967211993534d79d279b73465a9
Peter Ball <peterball.essex@live.com>
2026-07-25 19:38:10 +0100

    Set up example controller

---
+++ MusicOrganiserApi/Controllers/MathController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MusicOrganiserApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MathController : ControllerBase
+{
+    [HttpGet("add")]
+    public int Add([FromQuery] int a, [FromQuery] int b)
+    {
+        return a + b;
+    }
+}
--- MusicOrganiserApi/Program.cs
+++ MusicOrganiserApi/Program.cs
@@ -1,41 +1,26 @@
+const string CORS_POLICY_NAME = "MyCorsPolicy";
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(CORS_POLICY_NAME, policy =>
+        policy.WithOrigins("https://localhost:7293")
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.UseCors(CORS_POLICY_NAME);
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
